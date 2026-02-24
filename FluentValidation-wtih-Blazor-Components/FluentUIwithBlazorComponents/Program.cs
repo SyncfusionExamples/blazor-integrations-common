6d547f1ab3682c20757fef0eaa83d333b9973ab7
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2026-02-24 13:06:24 +0530

    Updated sample code

--- FluentValidation-wtih-Blazor-Components/FluentUIwithBlazorComponents/Program.cs
+++ FluentValidation-wtih-Blazor-Components/FluentUIwithBlazorComponents/Program.cs
@@ -1,11 +1,13 @@
 using FluentUIwithBlazorComponents.Components;
 using Syncfusion.Blazor;
+using Microsoft.FluentUI.AspNetCore.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddFluentUIComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
