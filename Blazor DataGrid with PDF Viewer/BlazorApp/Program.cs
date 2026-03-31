d901d3522f2add2d9863ba3dd7b53bb8cabb7259
VaseegaranSF4468 <162665726+VaseegaranSF4468@users.noreply.github.com>
2026-03-31 13:50:32 +0530

    1010972: Updated the Blazor DataGrid with PDF Viewer sample

--- Blazor DataGrid with PDF Viewer/BlazorApp/Program.cs
+++ Blazor DataGrid with PDF Viewer/BlazorApp/Program.cs
@@ -1,15 +1,17 @@
 using BlazorApp.Components;
 using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Register Syncfusion license key
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JHaF5cWWdCekx0Q3xbf1x2ZFdMYVRbQXNPMyBoS35RcEVgW3hecnVcR2dUVkVwVEFe");
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+// Configure SignalR to support large PDF file transfers
 builder.Services.AddSignalR(o => { o.MaximumReceiveMessageSize = 102400000; });
-
-builder.Services.AddMemoryCache();
 //Add Syncfusion Blazor service to the container.
+builder.Services.AddMemoryCache();
 builder.Services.AddSyncfusionBlazor();
 var app = builder.Build();
 
